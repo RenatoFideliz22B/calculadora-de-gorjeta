145a54219914356b252b034c338410df5be0b39d
Renato <renatofideliz@gmail.com>
2024-04-03 21:08:56 -0300

    Nono commit do CSS e terceiro commit do JS

--- script.js
+++ script.js
@@ -28,9 +28,17 @@ function receberQuantidadesPessoas(evento) {
 
 const botoesGorjeta = document.querySelectorAll(".gorjeta input[type='button']")
 botoesGorjeta.forEach(botao => {
-    botao.addEventListener("click", receberPorcentagem)
+    botao.addEventListener("click", receberPorcentagemBotao)
 })
 
 function receberPorcentagem(evento) {
+    botaoGorjeta.forEach(botao => {
+        botao.classList.remove("botao-ativo")
 
+        if (botao.value === evento.target.value) {
+            botao.classList.add("botao-ativo")
+        }
+    })
+
+    porcentagem = parseFloat(evento.target.value) / 100
 }
